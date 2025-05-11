0c8083e00587a7d2f5944e381865b8cbae84ac8e
K Karthik <karthik.kuramdasu@gmail.com>
2025-05-11 22:24:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://mydeb8.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
@@ -23,7 +23,7 @@ const io = new Server(httpServer, {
 
 // Configure CORS middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://mydeb8.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
